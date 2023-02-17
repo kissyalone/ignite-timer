e6eac613fc0e75547fe8b0db8d1b375bf599e8da
Jhonata <jhonata.pires@vitafor.com.br>
2023-02-17 16:44:39 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/themes/global';
+
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='danger'/>
+      <Button variant='success'/>
+      <Button/>
+
+      <GlobalStyle/>
+
+    </ThemeProvider>
+  )
+}
