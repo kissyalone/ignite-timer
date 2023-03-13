4c65e0f36ae9e82612b426bf9aff69995763e1e8
Jhonata <jhonata.pires@vitafor.com.br>
2023-03-13 14:00:47 -0300

    adicionando eslint e font google

--- src/styles/themes/global.ts
+++ src/styles/themes/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     * {
@@ -7,10 +7,21 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing:border-box;
     }
 
-    body {
-        color: #fff;
-        background: #333;
+    :focus {
+        outline: 0;
+        box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
     }
 
-    
+    body {
+        background: ${(props) => props.theme['gray-100']};
+        color: ${(props) => props.theme['gray-300']};
+    }
+
+    body, input, textarea, button {
+        font-family: 'roboto', 'sans-serif';
+        font-weight: 400;
+        font-size:1rem;
+    }
+
+
 `
