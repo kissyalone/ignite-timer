9a251bd5acd79f963ab8baef2fe6cde57a1eff9e
Jhonata <jhonata.pires@vitafor.com.br>
2023-03-16 14:06:50 -0300

    adicionando pasta de home, history e navlink no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import { HeaderContainer } from './styles'
+import {Timer, Scroll  } from 'phosphor-react'
+
 import logo from '../../assets/icon.svg'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={logo} alt="" />
       <nav>
-        <a href="">timer</a>
-        <a href="">history</a>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
+import { LayoutContainer } from './styles'
 import { Outlet } from 'react-router-dom'
-import { Header } from '../../components/Header/index'
+import { Header } from '../../components/Header'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
